4ee62ad16af284bbd9bbc0c35a443082c6ce824a
KRISHNA DESHMUKH <krishnadesh2001@gmail.com>
2023-02-18 10:25:43 +0530

    file1

---
+++ src/exceptionHandlig01/practise.java
@@ -0,0 +1,22 @@
+package exceptionHandlig01;
+
+public class practise{
+    public static void main(String[] args) {
+        int[] array  = {1,2,3};
+//        System.out.println(array[3]);
+        throw  new ArrayIndexOutOfBoundsException("Index Out Of Bounds");
+        // automatically identifies the problematic situation
+        // creates the ArrayIndexOutOfBoundsException class object
+        // then it is thrown to JRE
+        // JRE gives this object to the Default exception handler
+        // Default exception handler prints the exception object details
+        // Terminate the program.
+
+
+
+
+    }
+
+
+
+}
